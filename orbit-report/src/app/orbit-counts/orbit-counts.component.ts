e6d7a037c627db840fbd001bc7f3698c35b65566
JarvisF8 <michael_williams3@cable.comcast.com>
2021-06-18 17:11:45 -0400

    Orbit List Dynamic Count

--- orbit-report/src/app/orbit-counts/orbit-counts.component.ts
+++ orbit-report/src/app/orbit-counts/orbit-counts.component.ts
@@ -9,10 +9,76 @@ import { Satellite } from '../satellite';
 export class OrbitCountsComponent implements OnInit {
 
   @Input() satellites: Satellite[];
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+  addTotal(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      total += 1;
+    }
+    return total;
+  }
+  addDebris(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Space Debris') {
+        total += 1;
+      }
+    }
+    return total;
+  }
+
+  addCommunication(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Communication') {
+        total += 1;
+      }
+    }
+    return total;
+  }
+
+  addProbe(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Probe') {
+        total += 1;
+      }
+    }
+    return total;
+  }
+
+  addPositioning(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Positioning') {
+        total += 1;
+      }
+    }
+    return total;
+  }
+
+  addSpaceStation(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Space Station') {
+        total += 1;
+      }
+    }
+    return total;
+  }
+
+  addTelescope(): number {
+    let total: number = 0;
+    for(let i in this.satellites) {
+      if(this.satellites[i]['type'] === 'Telescope') {
+        total += 1;
+      }
+    }
+    return total;
   }
 
 }
