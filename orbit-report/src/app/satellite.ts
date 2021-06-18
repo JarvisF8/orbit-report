894bfb7f7bb18ce2128e5ecbfa8084beb995a074
JarvisF8 <michael_williams3@cable.comcast.com>
2021-06-18 12:10:55 -0400

    added debris style

--- orbit-report/src/app/satellite.ts
+++ orbit-report/src/app/satellite.ts
@@ -12,4 +12,11 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }
+
+    shouldShowWarning(): boolean {
+        if(this.type === 'Space Debris') {
+            return true;
+        }
+        return false;
+    }
 }
