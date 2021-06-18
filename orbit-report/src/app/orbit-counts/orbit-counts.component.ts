58e58697852c8ffb97e2f486acc87a563494342a
JarvisF8 <michael_williams3@cable.comcast.com>
2021-06-18 18:00:56 -0400

    Bonuses up to C

--- orbit-report/src/app/orbit-counts/orbit-counts.component.ts
+++ orbit-report/src/app/orbit-counts/orbit-counts.component.ts
@@ -9,76 +9,27 @@ import { Satellite } from '../satellite';
 export class OrbitCountsComponent implements OnInit {
 
   @Input() satellites: Satellite[];
+  type: string[] = ['', 'Space Debris', 'Communication', 'Probe', 'Positioning', 'Space Station', 'Telescope'];
+
 
   constructor() { }
 
-  ngOnInit() {
-  }
-  addTotal(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      total += 1;
-    }
-    return total;
-  }
-  addDebris(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Space Debris') {
-        total += 1;
-      }
-    }
-    return total;
-  }
+  ngOnInit() { }
 
-  addCommunication(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Communication') {
-        total += 1;
-      }
+  addMethod(input): number {
+    if(input === ''){
+      return this.satellites.length;
     }
-    return total;
-  }
-
-  addProbe(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Probe') {
-        total += 1;
+    else{
+      let total: number = 0;
+      for(let i in this.satellites) {
+        if(this.satellites[i].type === input) {
+          total++;
+        }
       }
+      return total;
     }
-    return total;
-  }
-
-  addPositioning(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Positioning') {
-        total += 1;
-      }
-    }
-    return total;
-  }
-
-  addSpaceStation(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Space Station') {
-        total += 1;
-      }
-    }
-    return total;
-  }
-
-  addTelescope(): number {
-    let total: number = 0;
-    for(let i in this.satellites) {
-      if(this.satellites[i]['type'] === 'Telescope') {
-        total += 1;
-      }
-    }
-    return total;
-  }
-
+  
+  }   
 }
+  
